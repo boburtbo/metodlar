f85a1fa67646317cc6ce6273090443e5807dd4b9
boburbo <bobur20081608@gmail.com>
2024-02-11 21:22:38 +0500

    metodlar

---
+++ app.js
@@ -0,0 +1,54 @@
+// let ism = 'bobur'
+// let yosh = '16'
+// console.log(ism,yosh);
+
+// yosh = 17
+
+// console.log(yosh);
+
+// const familiya = 'Botajonov'
+// console.log(familiya);
+
+// var joy = 'fargona'
+// joy = 'fargona tumani'
+// console.log(joy);
+
+// string 
+// console.log('hello world');
+
+// let email = 'bobur@gmail.com' 
+// console.log(email);
+
+// let firsName = 'Bobur'
+// let lastName = 'Botajonov'
+
+// let fullName = firsName + ' ' + lastName
+// console.log(fullName)
+
+// console.log(fullName[3]);
+
+// console.log(fullName.length);
+
+// console.log(fullName.toUpperCase());
+
+// let result= fullName.toLowerCase()
+// console.log(result);
+
+// let  index = fullName.indexOf()
+
+
+
+// let email = 'boburweb@gmail.com'
+// let result = email.indexOf('a')
+
+
+// let result = email.slice(9,10)
+// console.log(result);
+
+// let raidus = 10
+// const p1 = 3.14
+
+// console.log(raidus, p1);
+// let result = 12% 5
+// console.log(result);
+
